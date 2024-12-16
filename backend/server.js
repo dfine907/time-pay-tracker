5d059bdfb58104ee15675637603403cebd103d7f
Dina <dinagiacobbi@gmail.com>
2024-12-16 10:04:17 -0900

    Adding user model

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,20 @@ const express = require('express')
 const app = express()
 const connectDB = require('./db/connect')
 
+const port = process.env.PORT || 8000
+
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('HOME!')
 })
 
-const port = process.env.PORT || 8000
+app.get('api/v1/users')  // get all users
+app.post('api/v1/users') // create new user
+app.get('api/v1/users') // get single user
+app.patch('api/v1/users') // update a user
+app.delete('api/v1/users') // delete a user
+
 
 const start = async () => {
   try {
